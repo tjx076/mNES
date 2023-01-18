b8dcb2bc1c56520e78db924f6a3fae1271d7a880
tjx <grubby076@gmail.com>
2023-01-18 15:40:51 +0800

    指令

--- src/org/wrpg/mnes/IBus.java
+++ src/org/wrpg/mnes/IBus.java
@@ -3,8 +3,8 @@ package org.wrpg.mnes;
 public interface IBus {
 
     void writeByte(short address, byte data);
-    void writeWord(short address, byte[] data);
+    void writeWord(short address, short data);
     byte readByte(short address);
-    byte[] readWord(short address);
+    short readWord(short address);
 
 }
